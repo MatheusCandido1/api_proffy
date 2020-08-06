222f0102d034dc698bb060645bd75dd9b2cbed64
Matheus <matheus9bh@hotmail.com>
2020-08-06 14:30:35 -0300

    test-api

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    const users = [
+        { name: 'Matheus', age: 22 },
+        { name: 'Vini', age: 22},
+    ];
+
+    return response.json(users);   
+})
+
+app.listen(3333);
+
