d875b20eaa40300513a239b44863222b440c7b23
Matheus <matheus9bh@hotmail.com>
2020-08-06 21:41:48 -0300

    add-class-controller

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import ClassesController from './controllers/ClassesController';
+
+const routes = express.Router();
+const ClassesControllers = new ClassesController();
+
+routes.post('/classes', ClassesControllers.create);
+
+export default routes;
