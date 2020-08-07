e70e643a0ee4597bb20b52b4336ae7147c067143
Matheus <matheus9bh@hotmail.com>
2020-08-06 22:00:41 -0300

    add-index-classes

--- src/routes.ts
+++ src/routes.ts
@@ -6,4 +6,6 @@ const ClassesControllers = new ClassesController();
 
 routes.post('/classes', ClassesControllers.create);
 
+routes.get('/classes', ClassesControllers.index);
+
 export default routes;
